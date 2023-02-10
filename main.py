e3b8583eb5eedb4478529eef4eabc2a6c1e03995
Koralezy <titoff21@icloud.com>
2023-02-09 21:27:51 -0800

    added mute, kick, ban, and logs

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import discord  # pycord
+import discord  # pycord\
+from discord import Option
 from discord.ext import commands, tasks
 from discord.ext.commands import has_permissions
 from itertools import cycle
 import threading
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 from lists import foodgifs
 import random
 import json
@@ -35,17 +36,151 @@ async def change_status():
 
 # -------------------- Commands --------------------
 
+
 # -------------------- /mute --------------------
+
+@bot.command(description="Mutes someone for a certain amount of time.", pass_context=True)
+@has_permissions(moderate_members=True)
+async def mute(ctx, member: Option(discord.Member, description="Who you want to mute", required=True), reason: Option(str, required=False), days: Option(int, required=False), hours: Option(int, required=False), minutes: Option(int, required=False)):
+  if member.id == ctx.author.id:
+    await ctx.respond("You can't mute yourself!")
+    return
+  elif member.guild_permissions.manage_channels and not ctx.author.guild_permissions.manage_channels:
+    await ctx.respond("I think this is a staff member...")
+    return  
+
+  check=0
+  if reason != None:
+    r = f"`{reason}`"
+  else:
+    r = "`unspecified reason`"
+    reason = "unspecified reason"
+
+  if days != None:
+    d = f" {days} days,"
+  else:
+    d = ""
+    days = 0
+    check = check+1
+
+  if hours != None:
+    h = f" {hours} hours"
+  else:
+    h = ""
+    hours = 0
+    check = check+1
+
+  if minutes != None:
+    m = f" {minutes} minutes"
+  else:
+    m = ""
+    minutes = 0
+    check=check+1
+  
+  if check == 3:
+    await ctx.respond("**Mute unsuccessful!** Did you put in a duration?")
+    check=0
+    return
+  
+  check=0
+  duration = timedelta(days=days, hours=hours, minutes=minutes)
+  try:
+    await member.timeout_for(duration, reason=reason)
+  except:
+    await ctx.respond("I could not mute this member!")
+    return
+  embed = discord.Embed(title="Member Muted", color=discord.Color.red(), timestamp=datetime.utcnow())
+  embed.add_field(name="Member", value=f"{member.mention}", inline=True)
+  embed.add_field(name="Moderator", value=f"{ctx.author.mention}", inline=True)
+  embed.add_field(name="Duration", value=f"{d}{h}{m}", inline=True)
+  embed.add_field(name="Reason", value=f"{reason}", inline=True)
+
+  with open("logs.json", 'r') as f:
+    data = json.load(f)
+  logch = int(data[str(ctx.guild.id)])
+  print(logch)
+  logs = bot.get_channel(logch)
+  await logs.send(content=f"**Member muted** in <#{ctx.channel.id}> !", embed=embed)
+
+  await ctx.respond(f"{member.mention} has been timed out for**{d}{h}{m}** for **{r}.**")
+
+# -------------------- /unmute --------------------
 
 # -------------------- /kick --------------------
 
+@bot.command(description="Kicks someone.", pass_context=True)
+@has_permissions(kick_members=True)
+async def kick(ctx, member: Option(discord.Member, description="Who you want to kick", required=True), reason: Option(str, required=False)):
+  if member.id == ctx.author.id:
+    await ctx.respond("You can't kick yourself!")
+    return
+  if member.guild_permissions.manage_channels and not ctx.author.guild_permissions.manage_channels:
+    await ctx.respond("I think this is a staff member...")
+    return
+  if reason==None:
+    reason="unspecified reason"
+
+  try:
+    await member.kick(reason=reason)
+  except:
+    await ctx.respond("I could not kick this member!")  
+    return
+
+  embed = discord.Embed(title="Member Kicked", color=discord.Color.red(), timestamp=datetime.utcnow())
+  embed.add_field(name="Member", value=f"{member.mention}", inline=True)
+  embed.add_field(name="Moderator", value=f"{ctx.author.mention}", inline=True)
+  embed.add_field(name="Reason", value=f"{reason}", inline=True)
+
+
+  with open("logs.json", 'r') as f:
+    data = json.load(f)
+  logch = int(data[str(ctx.guild.id)])
+  logs = bot.get_channel(logch)
+  await logs.send(content=f"**Member kicked** in <#{ctx.channel.id}> !", embed=embed)
+
+  await ctx.respond(f"{member.mention} has been **kicked** for `{reason}`!")
+  
 # -------------------- /ban --------------------
+
+@bot.command(description="Bans someone.", pass_context=True)
+@has_permissions(ban_members=True)
+async def ban(ctx, member: Option(discord.Member, description="Who you want to ban", required=True)):
+  if member.id == ctx.author.id:
+    await ctx.respond("You can't ban yourself!")
+    return
+  if member.guild_permissions.manage_channels and not ctx.author.guild_permissions.manage_channels:
+    await ctx.respond("I think this is a staff member...")
+    return
+  if reason==None:
+    reason="unspecified reason"
+
+  try:
+    await member.ban(reason=reason)
+  except:
+    await ctx.respond("I could not ban this member!")
+    return  
+
+  embed = discord.Embed(title="Member Banned", color=discord.Color.red(), timestamp=datetime.utcnow())
+  embed.add_field(name="Member", value=f"{member.mention}", inline=True)
+  embed.add_field(name="Moderator", value=f"{ctx.author.mention}", inline=True)
+  embed.add_field(name="Reason", value=f"{reason}", inline=True)
+
+
+  with open("logs.json", 'r') as f:
+    data = json.load(f)
+  logch = int(data[str(ctx.guild.id)])
+  logs = bot.get_channel(logch)
+  await logs.send(content=f"**Member banned** in <#{ctx.channel.id}> !", embed=embed)
+
+  await ctx.respond(f"{member.mention} has been **banned** for `{reason}`!")
+
+# -------------------- /unban --------------------
 
 # -------------------- /purge --------------------
 
 @bot.command(description="Clears a specific amount of messages. Needs MANAGE MESSAGES permission.", pass_context=True)
 @has_permissions(manage_messages=True)
-async def purge(ctx, limit: discord.Option(int, description="How many messages you want to delete")):
+async def purge(ctx, limit: Option(int, description="How many messages you want to delete")):
   await ctx.channel.purge(limit=limit + 1)
   await ctx.respond('Cleared by {}'.format(ctx.author.mention), delete_after=3)
   await ctx.message.delete()
@@ -55,9 +190,20 @@ async def purge(ctx, limit: discord.Option(int, description="How many messages y
 @commands.has_permissions(manage_channels=True)
 async def lockdown(ctx):
   await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=False)
+
+  embed = discord.Embed(title="Channel Locked", color=discord.Color.red(), timestamp=datetime.utcnow())
+  embed.add_field(name="Channel", value=f"{ctx.channel.mention}", inline=True)
+  embed.add_field(name="Moderator", value=f"{ctx.author.mention}", inline=True)
+
+
+  with open("logs.json", 'r') as f:
+    data = json.load(f)
+  logch = int(data[str(ctx.guild.id)])
+  logs = bot.get_channel(logch)
+  await logs.send(content=f"<#{ctx.channel.id}> was locked!", embed=embed)
+
   await ctx.respond(
     ctx.channel.mention + " ***is now in lockdown.*** (Roles that have the [Send Messages] permission in this channel can still talk.)")
-
 
 # -------------------- /unlock --------------------
 @bot.command(description="Unlocks channel")
@@ -65,6 +211,16 @@ async def lockdown(ctx):
 async def unlock(ctx):
   try:
     await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=True)
+
+    embed = discord.Embed(title="Channel Unlocked", color=discord.Color.green(), timestamp=datetime.utcnow())
+    embed.add_field(name="Channel", value=f"{ctx.channel.mention}", inline=True)
+    embed.add_field(name="Moderator", value=f"{ctx.author.mention}", inline=True)
+
+    with open("logs.json", 'r') as f:
+      data = json.load(f)
+    logch = int(data[str(ctx.guild.id)])
+    logs = bot.get_channel(logch)
+    await logs.send(content=f"<#{ctx.channel.id}> was unlocked!", embed=embed)
     await ctx.respond(f"<#{ctx.channel.id}> ***has been unlocked.***")
   except:
     await ctx.respond(ctx.channel.mention + "is not locked. Check the channel permissions.")
@@ -99,7 +255,7 @@ async def food(ctx):
 
 # -------------------- /suggestfood --------------------
 @bot.command(description="(/food) Suggest a food gif to the dev.")
-async def suggestfood(ctx, suggestion: discord.Option(str, required=True)):
+async def suggestfood(ctx, suggestion: Option(str, required=True)):
   await ctx.defer()
   print(f"[/suggest food] Food Gif Suggestion: {suggestion} Suggester: {ctx.author}")
   await ragplas.send(f"*FOOD GIF SUGGESTION* | {ctx.author} gave a food gif suggestion: {suggestion}")
@@ -133,7 +289,7 @@ async def on_guild_channel_delete(channel):
   embed = discord.Embed(title="Channel Deleted",
                         description="No punishment issued.",
                         color=discord.Color.red())
-  embed.add_field(name="User", value=f"<@{entry.user.id}>", inline=True)
+  embed.add_field(name="User", value=f"{entry.user.mention}", inline=True)
   embed.add_field(name="Action", value="Deleted a channel.", inline=True)
   embed.add_field(name="Channel", value=channel.name, inline=True)
   embed.add_field(name="Guild", value=inguild, inline=True)
@@ -144,46 +300,42 @@ async def on_guild_channel_delete(channel):
   logch = int(data[str(channel.guild.id)])
   logs = bot.get_channel(logch)
   await logs.send(content=f"{entry.user} deleted {channel.name} in {channel.guild}!", embed=embed)
-  await ragplas.send(content=f"{entry.user} deleted {channel.name} in {channel.guild}!", embed=embed)
 
   global channelsdel
-  if channelsdel == 3:
+  if channelsdel == 5:
     member = entry.user
     membid = guild.get_member(entry.user.id)
-    if member != ragplas:
+    try:
+      for i in membid.roles:
+        try:
+          await membid.remove_roles(i)
+        except:
+          print(f"[Antiraid | Channels] Can't remove the role {i}")
+      await member.send(
+        f"Your roles were taken away in {inguild} because a raid was detected!")
+      punish = "quarantined"
 
-      try:
-        for i in membid.roles:
-          try:
-            await membid.remove_roles(i)
-          except:
-            print(f"[Antiraid | Channels] Can't remove the role {i}")
-        await member.send(
-          f"Your roles were taken away in {inguild} because a raid was detected, sorry! *False punishment? Message <@367436292276879362>*")
-        punish = "quarantined"
+    except:
+      print(f"[Antiraid | Channels] Could not find {member}'s roles")
+      await member.send(
+        f"You were kicked from {inguild} because a raid was detected!")
+      await guild.kick(member, reason="Deleted multiple channels (Anti-raid)")
+      punish = "kicked"
 
-      except:
-        print(f"[Antiraid | Channels] Could not find {member}'s roles")
-        await member.send(
-          f"You were kicked from {inguild} because a raid was detected, sorry! *False punishment? Message <@367436292276879362>*")
-        await guild.kick(member, reason="Deleted multiple channels (Anti-raid)")
-        punish = "kicked"
+    embed = discord.Embed(title="Raid Detected",
+                          description=f"Member {punish}.",
+                          color=discord.Color.red())
+    embed.add_field(name="User", value=f"{entry.user.mention}", inline=True)
+    embed.add_field(name="Action", value="Deleted a channel.", inline=True)
+    embed.add_field(name="Channel", value=channel.name, inline=True)
+    embed.add_field(name="Guild", value=inguild, inline=True)
+    embed.add_field(name="Date", value=f"<t:{int(pst_time)}>", inline=True)
 
-      embed = discord.Embed(title="Raid Detected",
-                            description=f"Member {punish}.",
-                            color=discord.Color.red())
-      embed.add_field(name="User", value=f"<@{entry.user.id}>", inline=True)
-      embed.add_field(name="Action", value="Deleted a channel.", inline=True)
-      embed.add_field(name="Channel", value=channel.name, inline=True)
-      embed.add_field(name="Guild", value=inguild, inline=True)
-      embed.add_field(name="Date", value=f"<t:{int(pst_time)}>", inline=True)
-
-      with open("logs.json", 'r') as f:
-        data = json.load(f)
-      logch = int(data[str(channel.guild.id)])
-      logs = bot.get_channel(logch)
-      await logs.send(content=f"**Raid detected** in {channel.guild}!", embed=embed)
-      await ragplas.send(content=f"**Raid detected** in {channel.guild}!", embed=embed)
+    with open("logs.json", 'r') as f:
+      data = json.load(f)
+    logch = int(data[str(channel.guild.id)])
+    logs = bot.get_channel(logch)
+    await logs.send(content=f"**Raid detected** in {channel.guild}!", embed=embed)
     channelsdel = 0
 
   else:
@@ -214,41 +366,39 @@ async def everyoneraid(message):
     pst_time = datetime.now().timestamp()
 
     global everyonepings
-    if everyonepings == 3:
+    if everyonepings == 5:
       member = message.author
       membid = guild.get_member(member)
-      if member != ragplas:
 
-        try:
-          for i in membid.roles:
-            try:
-              await membid.remove_roles(i)
-            except:
-              print(f"[Antiraid | @everyone] Can't remove the role {i}")
-          await member.send(
-            f"Your roles were taken away in {inguild} because a raid was detected, sorry! *False punishment? Message <@367436292276879362>*")
-          punish = "quarantined"
-        except:
-          print(f"[Antiraid | @everyone] Could not find {member}'s roles")
-          await member.send(
-            f"You were kicked from {inguild} because a raid was detected, sorry! *False punishment? Message <@367436292276879362>*")
-          await guild.kick(member, reason="Spammed @everyone")
-          punish = "kicked"
+      try:
+        for i in membid.roles:
+          try:
+            await membid.remove_roles(i)
+          except:
+            print(f"[Antiraid | @everyone] Can't remove the role {i}")
+        await member.send(
+          f"Your roles were taken away in {inguild} because a raid was detected!")
+        punish = "quarantined"
+      except:
+        print(f"[Antiraid | @everyone] Could not find {member}'s roles")
+        await member.send(
+          f"You were kicked from {inguild} because a raid was detected!")
+        await guild.kick(member, reason="Spammed @everyone")
+        punish = "kicked"
 
-        embed = discord.Embed(title="Raid Detected",
-                              description=f"Member {punish}.",
-                              color=discord.Color.red())
-        embed.add_field(name="User", value=f"<@{member.id}>", inline=True)
-        embed.add_field(name="Action", value="Spammed @everyone.", inline=True)
-        embed.add_field(name="Guild", value=inguild, inline=True)
-        embed.add_field(name="Date", value=f"<t:{int(pst_time)}>", inline=True)
+      embed = discord.Embed(title="Raid Detected",
+                            description=f"Member {punish}.",
+                            color=discord.Color.red())
+      embed.add_field(name="User", value=f"{member.mention}", inline=True)
+      embed.add_field(name="Action", value="Spammed @everyone.", inline=True)
+      embed.add_field(name="Guild", value=inguild, inline=True)
+      embed.add_field(name="Date", value=f"<t:{int(pst_time)}>", inline=True)
 
-        with open("logs.json", 'r') as f:
-          data = json.load(f)
-        logch = int(data[str(message.guild.id)])
-        logs = bot.get_channel(logch)
-        await logs.send(content=f"**Raid detected** in {inguild}!", embed=embed)
-        await ragplas.send(content=f"**Raid detected** in {inguild}!", embed=embed)
+      with open("logs.json", 'r') as f:
+        data = json.load(f)
+      logch = int(data[str(message.guild.id)])
+      logs = bot.get_channel(logch)
+      await logs.send(content=f"**Raid detected** in {inguild}!", embed=embed)
       everyonepings = 0
 
     else:
